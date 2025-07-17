7cc3d00a9740e1987ed422775c2e63e43ae91e4b
Josh Schonstal <josh@schonstal.org>
2025-07-16 20:35:50 -0700

    Add gdscript LSP support

--- init.lua
+++ init.lua
@@ -654,6 +654,7 @@ require('lazy').setup({
       --  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
       --  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
       local capabilities = require('blink.cmp').get_lsp_capabilities()
+      require('lspconfig').gdscript.setup(capabilities)
 
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
