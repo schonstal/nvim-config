a9f06adc52171c951237a2c4cce949fabae166d9
Josh Schonstal <josh@schonstal.org>
2025-08-07 18:38:48 -0700

    Add rounded corners to floating terminal

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -6,6 +6,10 @@ return {
       shade_terminals = false,
       open_mapping = '<leader>tt',
       direction = 'float',
+      float_opts = {
+        border = 'curved',
+        title_pos = 'left',
+      },
     }
     vim.keymap.set('n', '<leader>tv', '<cmd>:ToggleTerm direction=vertical size=80<CR>', { desc = 'Toggle Terminal [v]ertical' })
     vim.keymap.set('n', '<leader>th', '<cmd>:ToggleTerm direction=horizontal size=30<CR>', { desc = 'Toggle Terminal [h]orizontal' })
