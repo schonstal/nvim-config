819f98b9f1ea9cedfa8269506fc4562f4cd55d2e
Josh Schonstal <josh@schonstal.org>
2025-08-08 15:40:09 -0700

    Temporarily remove toggleterm keys until I figure out how to make them work better

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -11,7 +11,5 @@ return {
         title_pos = 'left',
       },
     }
-    vim.keymap.set('n', '<leader>tv', '<cmd>:ToggleTerm direction=vertical size=80<CR>', { desc = 'Toggle Terminal [v]ertical' })
-    vim.keymap.set('n', '<leader>th', '<cmd>:ToggleTerm direction=horizontal size=30<CR>', { desc = 'Toggle Terminal [h]orizontal' })
   end,
 }
