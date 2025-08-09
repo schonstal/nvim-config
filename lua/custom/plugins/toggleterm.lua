81195dc3267b41f3930ccaf2b09f01b348ed2a88
Josh Schonstal <josh@schonstal.org>
2025-08-08 17:42:07 -0700

    Implement basic GUT runner

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -11,5 +11,23 @@ return {
         title_pos = 'left',
       },
     }
+
+    -- TODO: Migrate this into a plugin that depends on toggleterm, it shouldn't be here.
+    vim.keymap.set('n', '<leader>tt', function()
+      local test_directory = vim.fn.expand '%:p:h'
+      local command = string.format('godot --headless -d -s addons/gut/gut_cmdln.gd -gdir=%s', test_directory)
+
+      local project_root = vim.fs.root(0, { 'project.godot' })
+      if project_root == nil then
+        -- In case the project file is in godot/
+        local project_file = vim.fs.find({ 'project.godot' })[1]
+        project_root = vim.fs.dirname(project_file)
+        if project_root == nil then
+          return -- We're not in a godot project
+        end
+      end
+
+      require('toggleterm').exec(command, 1, 0, project_root)
+    end, { desc = 'Test [T]his File' })
   end,
 }
