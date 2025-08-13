f14b9ba7b723fc2f7571be93001a05300c8f9f74
Josh Schonstal <josh@schonstal.org>
2025-08-13 16:27:02 -0700

    Fix sg keymap collision

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -99,7 +99,7 @@ return {
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-    vim.keymap.set('n', '<leader>sg', require('gdscript-extended-lsp').pick, { desc = '[S]earch [G]odot Classes' })
+    vim.keymap.set('n', '<leader>sc', require('gdscript-extended-lsp').pick, { desc = '[S]earch Godot [C]lasses' })
     vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
     -- Slightly advanced example of overriding default behavior and theme
