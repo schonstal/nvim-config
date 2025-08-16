35e261559e79f3b4459c48564f9edb5874090b6b
Josh Schonstal <josh@schonstal.org>
2025-08-15 18:15:33 -0700

    Improve picker behavior in godot project

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -11,7 +11,32 @@ return {
       enabled = true,
       timeout = 3000,
     },
-    picker = { enabled = false },
+    picker = {
+      enabled = false,
+      exclude = { '*.uid', '*.import' },
+      layout = {
+        reverse = true,
+        layout = {
+          box = 'horizontal',
+          backdrop = false,
+          width = 0.8,
+          height = 0.9,
+          border = 'none',
+          {
+            box = 'vertical',
+            { win = 'list', title = ' Results ', title_pos = 'center', border = 'rounded' },
+            { win = 'input', height = 1, border = 'rounded', title = '{title} {live} {flags}', title_pos = 'center' },
+          },
+          {
+            win = 'preview',
+            title = '{preview:Preview}',
+            width = 0.55,
+            border = 'rounded',
+            title_pos = 'center',
+          },
+        },
+      },
+    },
     lazygit = { enabled = true },
     quickfile = { enabled = true },
     dashboard = {
