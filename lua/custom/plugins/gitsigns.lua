a57e24de779110d97652ff5a5d0b999fc7f396e8
Josh Schonstal <josh@schonstal.org>
2025-08-20 10:06:17 -0700

    Disable gitsigns for now (distracting...)

--- lua/custom/plugins/gitsigns.lua
+++ lua/custom/plugins/gitsigns.lua
@@ -1,6 +1,7 @@
 -- Adds git related signs to the gutter, as well as utilities for managing changes
 return {
   'lewis6991/gitsigns.nvim',
+  enabled = false,
   opts = {
     signs = {
       add = { text = '+' },
