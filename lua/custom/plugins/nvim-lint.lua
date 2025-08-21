a9693a554617e427d9cd60ad5f5af3af863a13c2
Josh Schonstal <josh@schonstal.org>
2025-08-20 19:26:00 -0700

    Setup linting

---
+++ lua/custom/plugins/nvim-lint.lua
@@ -0,0 +1,27 @@
+return {
+  { -- Linting
+    'mfussenegger/nvim-lint',
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = function()
+      local lint = require 'lint'
+      lint.linters_by_ft = {
+        markdown = { 'vale' },
+        gdscript = { 'gdlint' },
+        rst = { 'vale' },
+        text = { 'vale' },
+      }
+
+      -- Create autocommand which carries out the actual linting
+      -- on the specified events.
+      local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+        group = lint_augroup,
+        callback = function()
+          if vim.bo.modifiable then
+            lint.try_lint()
+          end
+        end,
+      })
+    end,
+  },
+}
